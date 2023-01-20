fb24f23a183955fb8645945831ecf4f3f788ef11
Marysia Hano <blondyneczka@gmail.com>
2023-01-20 11:52:07 +0100

    Calculator class with modified in test.

--- src/test/java/Lesson2/Excersise3/CalculatorTest.java
+++ src/test/java/Lesson2/Excersise3/CalculatorTest.java
@@ -1,22 +1,31 @@
 package Lesson2.Excersise3;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
     @Test
-    void sum() {
+    void shouldTwoValues1and1() {
+        int sum = Calculator.sum(1,1);
+        Assertions.assertEquals(2, sum);
     }
 
     @Test
-    void subtract() {
+    void shouldDeductTwoValues5and3() {
+        int subtract = Calculator.subtract(5, 3);
+        Assertions.assertEquals(2, subtract);
     }
 
     @Test
-    void multiply() {
+    void shouldMultiplyTwoValues2and3() {
+        int multiply = Calculator.multiply(2, 3);
+        Assertions.assertEquals(6, multiply);
     }
 
     @Test
-    void division() {
+    void shouldDivisionTwoValues6and3() {
+        int division = Calculator.division(6, 3);
+        Assertions.assertEquals(2, division);
     }
 }
