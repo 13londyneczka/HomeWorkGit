0223e10b200eb5edb75e65173e4082a80230d9db
Marysia Hano <blondyneczka@gmail.com>
2023-01-20 10:54:50 +0100

    Add Class Calculator and empty test

---
+++ src/main/java/Lesson2/Excersise3/Calculator.java
@@ -0,0 +1,23 @@
+package Lesson2.Excersise3;
+
+public class Calculator {
+    public static int sum(int value1, int value2) {
+        int result = value1 + value2;
+        return result;
+    }
+
+    public static int subtract(int a, int b) {
+        int result = a - b;
+        return result;
+    }
+
+    public static int multiply(int a, int b) {
+        int result = a * b;
+        return result;
+    }
+
+    public static int division(int a, int b) {
+        int result = a / b;
+        return result;
+    }
+}
---
+++ src/test/java/Lesson2/Excersise3/CalculatorTest.java
@@ -0,0 +1,22 @@
+package Lesson2.Excersise3;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void sum() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void division() {
+    }
+}
