6d462181c6f5b88d18b4aa05444d22e4d95d27d9
Marysia Hano <blondyneczka@gmail.com>
2023-01-20 12:24:17 +0100

    Create project with Maven structure.

---
+++ src/main/java/Lesson2/Exercise2/Calculator.java
@@ -0,0 +1,9 @@
+package Lesson2.Exercise2;
+
+public class Calculator {
+
+    public static int sum(int value1, int value2){
+        int result = value1 + value2;
+        return result;
+    }
+}
