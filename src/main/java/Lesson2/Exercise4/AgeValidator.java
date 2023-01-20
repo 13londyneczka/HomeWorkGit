8f56c177dd30fb318fdaa4780ab1bd3f9925e377
Marysia Hano <blondyneczka@gmail.com>
2023-01-20 11:04:43 +0100

    Age Class and test

---
+++ src/main/java/Lesson2/Exercise4/AgeValidator.java
@@ -0,0 +1,12 @@
+package Lesson2.Exercise4;
+
+public class AgeValidator {
+
+    public static boolean isAdult (int a) {
+        if (a >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
