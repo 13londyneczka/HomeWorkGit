5e36cdfa6030a1f344d04bb0a4f865a3929141c2
Marysia Hano <blondyneczka@gmail.com>
2023-01-20 11:24:43 +0100

    Place in cinema

---
+++ src/main/java/Lesson3/Exercise1/PlaceInCinema.java
@@ -0,0 +1,30 @@
+package Lesson3.Exercise1;
+
+public class PlaceInCinema {
+
+   private final int number;
+   private final int row;
+   private boolean isFree;
+
+    public PlaceInCinema(int number, int row, boolean isFree) {
+        this.number = number;
+        this.row = row;
+        this.isFree = isFree;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public boolean isFree() {
+        return isFree;
+    }
+
+    public void setFree(boolean free) {
+        isFree = free;
+    }
+}
