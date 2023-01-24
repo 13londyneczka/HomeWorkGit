5c5f8f60845d1b302572a037b745a24436d4dc08
Marysia Hano <blondyneczka@gmail.com>
2023-01-24 22:19:29 +0100

    Class with Even and Odd number and test modified.

--- src/main/java/Lesson2/Exercise5/EvenAndOddNumber.java
+++ src/main/java/Lesson2/Exercise5/EvenAndOddNumber.java
@@ -1,11 +1,11 @@
 package Lesson2.Exercise5;
 
 public class EvenAndOddNumber {
-    public static boolean isEvenNumber (int a) {
-        return a % 2 == 0;
+    public static boolean isEvenNumber (int number) {
+        return number % 2 == 0;
     }
-    public static boolean isOddNumber (int a) {
-        return a % 2 != 0;
+    public static boolean isOddNumber (int number) {
+        return number % 2 != 0;
     }
 
 }
--- src/test/java/Lesson2/Exercise5/EvenAndOddNumberTest.java
+++ src/test/java/Lesson2/Exercise5/EvenAndOddNumberTest.java
@@ -8,12 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class EvenAndOddNumberTest {
 
     @Test
-    void isEvenNumber() {
+    void shouldBeEven() {
         boolean isEvenNumber = EvenAndOddNumber.isEvenNumber(6);
         Assertions.assertEquals(true, isEvenNumber);
     }
         @Test
-    void isOddNumber() {
+    void shouldBeOdd() {
         boolean isOddNumber = EvenAndOddNumber.isOddNumber(5);
         Assertions.assertEquals(true, isOddNumber);
     }
