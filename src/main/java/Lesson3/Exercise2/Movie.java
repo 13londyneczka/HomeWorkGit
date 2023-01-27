dc36d6ada1f4bd34e9912787a96409503612d510
Marysia Hano <blondyneczka@gmail.com>
2023-01-27 12:45:25 +0100

    Modified Class Movie.

--- src/main/java/Lesson3/Exercise2/Movie.java
+++ src/main/java/Lesson3/Exercise2/Movie.java
@@ -3,28 +3,33 @@ package Lesson3.Exercise2;
         private final String title;
         private final String directorName;
         private final String directorSurname;
-        private final int movieDuration;
+        private final int movieDurationInMinutes;
         private final int yearOfProduction;
 
         public Movie(String title, String directorName, String directorSurname, int movieDuration, int yearOfProduction) {
             this.title = title;
             this.directorName = directorName;
             this.directorSurname = directorSurname;
-            this.movieDuration = movieDuration;
+            this.movieDurationInMinutes = movieDuration;
             this.yearOfProduction = yearOfProduction;
         }
+
         public String getTitle(){
             return title;
         }
+
         public String getName(){
             return directorName;
         }
+
         public String getSurname(){
             return directorSurname;
         }
+
         public int getMovieDuration(){
-            return movieDuration;
+            return movieDurationInMinutes;
         }
+
         public int getYearOfProduction(){
             return yearOfProduction;
         }
---
+++ src/main/java/Lesson3/Exercise3/BaseEmployee.java
@@ -0,0 +1,28 @@
+package Lesson3.Exercise3;
+
+public abstract class BaseEmployee {
+    private static final int BASE_SALARY = 3000;
+    private final String name;
+    private final String surname;
+    private final int employmentYear;
+    private final int salary;
+
+    public BaseEmployee(String name, String surname, int employmentYear, int salary) {
+
+        this.name = name;
+        this.surname = surname;
+        this.employmentYear = employmentYear;
+        this.salary = salary;
+    }
+
+    public BaseEmployee(String name, String surname, int employmentYear) {
+        this.name = name;
+        this.surname = surname;
+        this.employmentYear = employmentYear;
+        this.salary = BASE_SALARY;
+    }
+    public int getSalary(){
+        return this.salary;
+    }
+    public abstract int calculateMonthlySalary();
+}
