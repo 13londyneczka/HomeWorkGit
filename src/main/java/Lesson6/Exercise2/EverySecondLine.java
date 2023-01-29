36fea66354220705b4859c768727d66137672df6
Marysia Hano <blondyneczka@gmail.com>
2023-01-29 02:53:02 +0100

    Add Lesson 6 Exercise 2.

---
+++ src/main/java/Lesson6/Exercise2/EverySecondLine.java
@@ -0,0 +1,25 @@
+package Lesson6.Exercise2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class EverySecondLine {
+
+    public static void main(String[] args) {
+        String filePath = "data.txt";
+        printEverySecondLine(filePath);
+    }
+
+    private static void printEverySecondLine(String filePath) {
+        try {
+            String fileContent = Files.readString(Path.of(filePath));
+            String[] lines = fileContent.split("\n");
+            for (int i = 0; i < lines.length; i += 2) {
+                System.out.println(lines[1]);
+            }
+        }catch(IOException e){
+                throw new RuntimeException(e);
+        }
+    }
+}
