0a829a25811a4a31d446ad62e043ab0d51981007
Marysia Hano <blondyneczka@gmail.com>
2023-01-29 03:12:49 +0100

    Add Lesson 6 Exercise 5.

---
+++ src/main/java/Lesson6/Exercise5/ReverseOrder.java
@@ -0,0 +1,25 @@
+package Lesson6.Exercise5;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ReverseOrder {
+
+    public static void getReversOrder(String inputFile) {
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get(inputFile));
+            printReversePart(allLines);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printReversePart(List<String> allLines){
+        for (int i = allLines.size() - 1; i > 0; i --) {
+            System.out.println(allLines.get(i));
+        }
+    }
+}
