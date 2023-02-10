602d96970ec6d543db0c7c303fbeee80fff4c421
Marysia Hano <blondyneczka@gmail.com>
2023-02-10 02:10:28 +0100

    Modified lesson 3 exercise 2. Task movie.

--- src/main/java/Lesson3/Exercise2/Movie.java
+++ src/main/java/Lesson3/Exercise2/Movie.java
@@ -1,36 +1,37 @@
 package Lesson3.Exercise2;
-    public class Movie {
-        private final String title;
-        private final String directorName;
-        private final String directorSurname;
-        private final int movieDurationInMinutes;
-        private final int yearOfProduction;
 
-        public Movie(String title, String directorName, String directorSurname, int movieDuration, int yearOfProduction) {
-            this.title = title;
-            this.directorName = directorName;
-            this.directorSurname = directorSurname;
-            this.movieDurationInMinutes = movieDuration;
-            this.yearOfProduction = yearOfProduction;
-        }
+public class Movie {
+    private final String title;
+    private final String directorName;
+    private final String directorSurname;
+    private final int movieDurationInMinutes;
+    private final int yearOfProduction;
 
-        public String getTitle(){
-            return title;
-        }
-
-        public String getName(){
-            return directorName;
-        }
-
-        public String getSurname(){
-            return directorSurname;
-        }
-
-        public int getMovieDuration(){
-            return movieDurationInMinutes;
-        }
-
-        public int getYearOfProduction(){
-            return yearOfProduction;
-        }
+    public Movie(String title, String directorName, String directorSurname, int movieDurationInMinutes, int yearOfProduction) {
+        this.title = title;
+        this.directorName = directorName;
+        this.directorSurname = directorSurname;
+        this.movieDurationInMinutes = movieDurationInMinutes;
+        this.yearOfProduction = yearOfProduction;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public String getDirectorSurname() {
+        return directorSurname;
+    }
+
+    public int getMovieDurationInMinutes() {
+        return movieDurationInMinutes;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+}
