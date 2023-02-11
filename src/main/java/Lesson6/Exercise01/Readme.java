2fa3211bd568dc9d6256a90109281d773ac9a2ab
Marysia Hano <blondyneczka@gmail.com>
2023-02-11 13:36:17 +0100

    Modified class Readme.

--- src/main/java/Lesson6/Exercise01/Readme.java
+++ src/main/java/Lesson6/Exercise01/Readme.java
@@ -1,10 +1,19 @@
 package Lesson6.Exercise01;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Readme {
     public static void main(String[] args) {
         File file = new File("D:\\data.txt");
-        System.out.println(file.getName());
+
+        try {
+            String text = Files.readString(Paths.get("D:\\data.txt"));
+            System.out.println(text);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
