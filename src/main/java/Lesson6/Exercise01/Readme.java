9086a5c938512ee063b3549e6863b6f637ce466d
Marysia Hano <blondyneczka@gmail.com>
2023-02-11 13:31:36 +0100

    Add lesson 6 exercise 01.

---
+++ src/main/java/Lesson6/Exercise01/Readme.java
@@ -0,0 +1,10 @@
+package Lesson6.Exercise01;
+
+import java.io.File;
+
+public class Readme {
+    public static void main(String[] args) {
+        File file = new File("D:\\data.txt");
+        System.out.println(file.getName());
+    }
+}
