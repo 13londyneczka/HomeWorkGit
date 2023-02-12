2b4e16d0e8e45eda9e509cf85d35cc2ac05d9a85
Marysia Hano <blondyneczka@gmail.com>
2023-02-12 13:38:55 +0100

    Modified task. Add resources TextFile.

--- src/main/java/Lesson6/Exercise5/ReverseOrder.java
+++ src/main/java/Lesson6/Exercise5/ReverseOrder.java
@@ -1,5 +1,6 @@
 package Lesson6.Exercise5;
 
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -7,19 +8,24 @@ import java.util.List;
 
 public class ReverseOrder {
 
-    public static void PrintReversOrder(String inputFile) {
-        try {
-            List<String> allLines = Files.readAllLines(Paths.get(inputFile));
-            printReversePart(allLines);
-
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\Marysia\\Desktop\\GitRepozytoria\\HomeWorkGit\\src\\main\\resources\\TextFile";
+        PrintReversOrder("C:\\Users\\Marysia\\Desktop\\GitRepozytoria\\HomeWorkGit\\src\\main\\resources\\TextFile");
     }
 
-    private static void printReversePart(List<String> allLines){
-        for (int i = allLines.size() - 1; i > 0; i --) {
-            System.out.println(allLines.get(i));
+        public static void PrintReversOrder(String inputFile) {
+            try {
+                List<String> allLines = Files.readAllLines(Paths.get("C:\\Users\\Marysia\\Desktop\\GitRepozytoria\\HomeWorkGit\\src\\main\\resources\\TextFile"));
+                printReversePart(allLines);
+
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+
+        private static void printReversePart(List<String> allLines) {
+            for (int i = allLines.size() -1; i > -1; i--) {
+                System.out.println(allLines.get(i));
+            }
         }
     }
-}
