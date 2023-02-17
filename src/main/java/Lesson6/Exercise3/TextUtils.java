f61b30ca51a5b191145a95d14763285bd3b472c6
Marysia Hano <blondyneczka@gmail.com>
2023-02-17 15:49:33 +0100

    Modified task.

--- src/main/java/Lesson6/Exercise3/TextUtils.java
+++ src/main/java/Lesson6/Exercise3/TextUtils.java
@@ -6,13 +6,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class TextUtils {
-    public static void displayTheNumbersOfCharacters(String pathToFile) {
-        File file = new File("D:\\data.txt");
-        try {
-            String text = Files.readString(Paths.get("D:\\data.txt"));
-            System.out.println(file.length());
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+  public static void displayTheNumbersOfCharacters(String pathname) {
+    File file = new File("D:\\data.txt");
+    try {
+      Files.readString(Paths.get("D:\\data.txt"));
+      System.out.println(file.length());
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
+  }
 }
