a043a2504fcd3640273f0ab19f12e22cd57961c9
Marysia Hano <blondyneczka@gmail.com>
2023-02-17 17:04:38 +0100

    Modified task.

--- src/main/java/Lesson6/Exercise5/ReverseOrder.java
+++ src/main/java/Lesson6/Exercise5/ReverseOrder.java
@@ -8,23 +8,23 @@ import java.util.List;
 
 public class ReverseOrder {
 
-    public static void main(String[] args) {
-        printReversOrder("C:\\Users\\Marysia\\Desktop\\GitRepozytoria\\HomeWorkGit\\src\\main\\resources\\TextFile");
+  public static void main(String[] args) {
+    printReversOrder("C:\\Users\\Marysia\\Desktop\\GitRepozytoria\\HomeWorkGit\\src\\main\\resources\\TextFile");
+  }
+
+  public static void printReversOrder(String pathName) {
+    try {
+      List<String> allLines = Files.readAllLines(Paths.get(pathName));
+      printReversePart(allLines);
+
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
+  }
 
-        public static void printReversOrder(String pathName) {
-            try {
-                List<String> allLines = Files.readAllLines(Paths.get(pathName));
-                printReversePart(allLines);
-
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-        }
-
-        private static void printReversePart(List<String> allLines) {
-            for (int i = allLines.size() -1; i >= 0; i--) {
-                System.out.println(allLines.get(i));
-            }
-        }
+  private static void printReversePart(List<String> allLines) {
+    for (int i = allLines.size() - 1; i >= 0; i--) {
+      System.out.println(allLines.get(i));
     }
+  }
+}
