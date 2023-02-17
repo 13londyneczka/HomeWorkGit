1d279092c58eca350f9c2bcbea4048a708b9e820
Marysia Hano <blondyneczka@gmail.com>
2023-02-17 16:37:03 +0100

    Modified task.

--- src/main/java/Lesson6/Exercise01/WritingContent.java
+++ src/main/java/Lesson6/Exercise01/WritingContent.java
@@ -1,20 +1,18 @@
 package Lesson6.Exercise01;
 
-import java.io.File;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class WritingContent {
 
-    public static void displayContent(String pathToFile) {
-        File file = new File("D:\\data.txt");
-
-        try {
-            String text = Files.readString(Paths.get("D:\\data.txt"));
-            System.out.println(text);
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+  public static void displayContent(String pathName) {
+    try {
+      String text = Files.readString(Paths.get(pathName));
+      System.out.println(text);
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
+  }
 }
