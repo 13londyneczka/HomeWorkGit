3f7bcbfef892b19afbf21b6bf96988fd7491508d
Marysia Hano <blondyneczka@gmail.com>
2023-02-19 00:06:57 +0100

    Modified.

--- src/main/java/Lesson6/Exercise9/Main.java
+++ src/main/java/Lesson6/Exercise9/Main.java
@@ -5,6 +5,6 @@ import static Lesson6.Exercise9.Validation.validate;
 
 public class Main {
   public static void main(String [] args){
-    validate(3);
+    validate(5);
   }
 }
--- src/main/java/Lesson6/Exercise9/Validation.java
+++ src/main/java/Lesson6/Exercise9/Validation.java
@@ -1,11 +1,18 @@
 package Lesson6.Exercise9;
 
-public class Validation extends Main{
-  public static void validate(int i) {
-    if (i % 3 == 0) {
+public class Validation {
+  public static void validate(int number) {
+    if (number % 3 == 0) {
       System.out.println("Liczba jest podzielna przez 3");
-    } else if (i % 3 != 0) {
-      System.out.println("Liczba nie jest podzielna przez 3");
+    } else if (number % 3 != 0) {
+      throw new IllegalArgumentException(" Liczba jest niepodzielna przez 3");
+    }
+
+    Validation validation = new Validation(validate(number));
+    try {
+      validation.validate(number);
+    } catch (IllegalArgumentException){
+      exception.printStackTrace();
     }
   }
 }
