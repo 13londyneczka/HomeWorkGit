9b7a4da48e8e63c2cc08aa159069111eb6a98380
Marysia Hano <blondyneczka@gmail.com>
2023-02-22 13:25:56 +0100

    Modified.

--- src/main/java/Lesson9/Exercise5/BubbleSort.java
+++ src/main/java/Lesson9/Exercise5/BubbleSort.java
@@ -3,15 +3,14 @@ package Lesson9.Exercise5;
 import java.util.List;
 
 public class BubbleSort<T extends Comparable<T>> {
-
   public void sort(List<T> list) {
-    int n = list.size();
-
-    for (int i = 0; i < n - 1; i++) {
-      if (list.get(i).compareTo(list.get(i + 1)) > 0) {
-        T temp = list.get(i);
-        list.set(i, list.get(i + 1));
-        list.set(i + 1, temp);
+    for (int i = 0; i < list.size() - 1; i++) {
+      for (int j = 0; j < list.size() - i - 1; j++) {
+        if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+          T temp = list.get(j);
+          list.set(j, list.get(j + 1));
+          list.set(j + 1, temp);
+        }
       }
     }
   }
