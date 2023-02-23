ee16231ff861eeb4463537dcc6684674045caf35
Marysia Hano <blondyneczka@gmail.com>
2023-02-23 15:50:14 +0100

    Add Lesson 10 Exercise 6.

---
+++ src/main/java/Lesson10/Exercise6/BirthDate.java
@@ -0,0 +1,26 @@
+package Lesson10.Exercise6;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+
+public class BirthDate {
+
+  public static  int getAge(LocalDate dateOfBirth) {
+    LocalDate dateNow = LocalDate.now();
+    int age = dateNow.getYear()  - dateOfBirth.getYear();
+    int currentDay = dateNow.getDayOfMonth();
+    int dayOfBirthday = dateOfBirth.getDayOfMonth();
+    int currentMonth = dateNow.getMonthValue();
+    int monthOfBirthday = dateOfBirth.getMonthValue();
+    if(currentDay >= dayOfBirthday && currentMonth >= monthOfBirthday) {
+      return age;
+    } else return age -1;
+  }
+
+  public static int getWeekOfYear(LocalDate dateOfBirth) {
+    int week = dateOfBirth.get(ChronoField.ALIGNED_WEEK_OF_YEAR);
+    return week;
+  }
+
+  public static String getDayOfBirth()
+}
