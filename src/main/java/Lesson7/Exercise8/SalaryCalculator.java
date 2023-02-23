3922b8862d49c11124ec3edc9f74581d3b528a87
Marysia Hano <blondyneczka@gmail.com>
2023-02-23 14:15:55 +0100

    Add Lesson 7 Exercise 8.

---
+++ src/main/java/Lesson7/Exercise8/SalaryCalculator.java
@@ -0,0 +1,17 @@
+package Lesson7.Exercise8;
+
+public class SalaryCalculator {
+
+  private WeekendWorkProvider weekendWorkProvider;
+
+  private WorkDayProvider workDayProvider;
+
+  public SalaryCalculator(WeekendWorkProvider weekendWorkProvider, WorkDayProvider workDayProvider) {
+    this.weekendWorkProvider = weekendWorkProvider;
+    this.workDayProvider = workDayProvider;
+  }
+
+  public double getSalary(int workedDaysAmount, int weekendWorkedDays, double extraBonus) {
+    return workDayProvider.getSalary(workedDaysAmount) + weekendWorkProvider.getWeekendSalary(weekendWorkedDays) + extraBonus;
+  }
+}
