3ade877270e1cc0d7bfef5175a880c61abd6cc2c
Marysia Hano <blondyneczka@gmail.com>
2023-02-23 19:13:00 +0100

    Modified.

---
+++ src/main/java/Lesson6/Exercise7/Main.java
@@ -0,0 +1,12 @@
+package Lesson6.Exercise7;
+
+
+import static Lesson6.Exercise7.SquareRoot.square;
+
+
+public class Main {
+
+  public static void main (String [] args){
+    square(10);
+  }
+}
--- src/main/java/Lesson6/Exercise7/SquareRoot.java
+++ src/main/java/Lesson6/Exercise7/SquareRoot.java
@@ -3,11 +3,12 @@ package Lesson6.Exercise7;
 import static java.lang.Math.sqrt;
 
 public class SquareRoot {
-  public static double sqrt(int number) {
+  public static String square(int number) {
     if (number < 0) {
       throw new IllegalArgumentException("Z liczby ujemnej nie można zrobić pierwiastka");
-    } else {
-      return sqrt(number);
+    } else if(number >= 0){
+      System.out.println(sqrt(number));
     }
+    return null;
   }
 }
