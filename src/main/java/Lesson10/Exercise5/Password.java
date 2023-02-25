f40e71bc2a0f02e7376c4e3d9289bc763352c5d6
Marysia Hano <blondyneczka@gmail.com>
2023-02-25 22:09:34 +0100

    Modified.

--- src/main/java/Lesson10/Exercise5/Password.java
+++ src/main/java/Lesson10/Exercise5/Password.java
@@ -6,9 +6,8 @@ import java.util.regex.Pattern;
 public class Password {
 
     public static boolean isPasswordGood(String password) {
-        Pattern pattern= Pattern.compile("^(?=.*\\d)(?=>*[A-Z])(?!>*?(?:23))>{7,}&");
+        Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[A-Z])(?!.*?(?:23)).{7,}$");
         Matcher matcher = pattern.matcher(password);
-        boolean matches = matcher.matches();
-        return matches;
+        return matcher.matches();
     }
 }
--- src/test/java/Lesson10/Exercise5/PasswordTest.java
+++ src/test/java/Lesson10/Exercise5/PasswordTest.java
@@ -1,16 +1,14 @@
 package Lesson10.Exercise5;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 
 class PasswordTest {
-
   @Test
-  void shouldThePasswordGood() {
-    //given
-
-
+  void shouldBeMatchPassword() {
+    String password = "W1sdfsdfsdfsdf";
+    assertTrue(Password.isPasswordGood(password));
   }
 }
