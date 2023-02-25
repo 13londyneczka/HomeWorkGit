97e0ca3dcb22623be9bd3e3d1be9a01e156f4f74
Marysia Hano <blondyneczka@gmail.com>
2023-02-25 14:07:08 +0100

    Modified.

--- src/main/java/Lesson6/Exercise7/SquareRoot.java
+++ src/main/java/Lesson6/Exercise7/SquareRoot.java
@@ -3,12 +3,11 @@ package Lesson6.Exercise7;
 import static java.lang.Math.sqrt;
 
 public class SquareRoot {
-  public static String square(int number) {
+  public static double square(int number) {
     if (number < 0) {
       throw new IllegalArgumentException("Z liczby ujemnej nie można zrobić pierwiastka");
-    } else if(number >= 0){
-      System.out.println(sqrt(number));
+    } else {
+      return Math.sqrt(number);
     }
-    return null;
   }
 }
