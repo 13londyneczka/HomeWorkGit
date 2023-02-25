7e3336d2a22fc7d70e3d54d7c5f7803cf6035435
Marysia Hano <blondyneczka@gmail.com>
2023-02-25 19:41:42 +0100

    Modified.

---
+++ src/test/java/Lesson10/Exercise5/PasswordTest.java
@@ -0,0 +1,16 @@
+package Lesson10.Exercise5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.regex.Pattern;
+import org.junit.jupiter.api.Test;
+
+class PasswordTest {
+
+  @Test
+  void shouldThePasswordGood() {
+    //given
+
+
+  }
+}
